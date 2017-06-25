13b602ccf5198cbbe3317e46771d74859422c982
Andrei Sousa <sousandrei@gmail.com>
2017-06-25 16:07:17 -0300

    settings + drawer

--- app/app/src/main/java/com/akuntsu/instransactions/acts/OfferActivity.java
+++ app/app/src/main/java/com/akuntsu/instransactions/acts/OfferActivity.java
@@ -1,15 +1,22 @@
 package com.akuntsu.instransactions.acts;
 
 import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.akuntsu.instransactions.Config;
 import com.akuntsu.instransactions.R;
@@ -43,10 +50,21 @@ public class OfferActivity extends AppCompatActivity implements AddOfferFragment
 
     List<Offer> offerList = new ArrayList<>();
 
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
+
+        mPlanetTitles = getResources().getStringArray(R.array.drawer_string_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.offer_layout);
+        mDrawerList = (ListView) findViewById(R.id.drawer_layout);
+
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, mPlanetTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -61,7 +79,6 @@ public class OfferActivity extends AppCompatActivity implements AddOfferFragment
             }
         });
 
-
         context = this;
 
         queue = Volley.newRequestQueue(context);
@@ -75,6 +92,35 @@ public class OfferActivity extends AppCompatActivity implements AddOfferFragment
         mAdapter = new OfferAdapter(context, offerList);
         mRecyclerView.setAdapter(mAdapter);
         getOffers();
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        Intent homeAct;
+        switch (position) {
+            case 0:
+                homeAct = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(homeAct);
+                break;
+            case 1:
+                homeAct = new Intent(getBaseContext(), SettingsActivity.class);
+                startActivity(homeAct);
+                break;
+            case 2:
+                homeAct = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(homeAct);
+                break;
+            case 3:
+                homeAct = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(homeAct);
+                break;
+        }
     }
 
     private void getOffers() {
